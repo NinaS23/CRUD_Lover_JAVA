4ac1a83ff6078ff1e5fdfb045e5f0a89c04d94de
DUDA <mariabevenuto062@gmail.com>
2023-04-03 16:13:44 -0300

    feat: delete car by id

--- src/main/java/com/cars/api/controllers/CarsController.java
+++ src/main/java/com/cars/api/controllers/CarsController.java
@@ -2,7 +2,9 @@ package com.cars.api.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,10 +28,17 @@ public class CarsController {
     }
 
     @GetMapping
-    public List<Car> GetAllCarsRegisters(){
+    public List<Car> GetAllCarsRegisters() {
         List<Car> cars = repository.findAll();
-         return cars;   
+        return cars;
     }
 
-}
+    @DeleteMapping("/{id}")
+    public String DeleteCarById(@PathVariable Long id) {
+        repository.deleteById(id);
+        String sucess = " Car " + id + " has been deleted ";
+        return sucess;
+    }
 
+
+}
