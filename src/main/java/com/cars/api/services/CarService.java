c2f8297f5cf2e12f468feffc6812b5224428d2d2
DUDA <mariabevenuto062@gmail.com>
2023-04-03 17:09:01 -0300

    feat: service folder

---
+++ src/main/java/com/cars/api/services/CarService.java
@@ -0,0 +1,40 @@
+package com.cars.api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cars.api.repository.CarRepository;
+import com.cars.api.model.Car;
+
+@Service
+public class CarService {
+
+    @Autowired
+	private CarRepository repository;
+
+
+    public void RegisterCar(Car data) {
+       repository.save(data);
+    }
+
+    public List<Car> GetAllCarsRegisters() {
+        List<Car> cars = repository.findAll();
+        return cars;
+    }
+
+    public String DeleteCarById(@PathVariable Long id) {
+        repository.deleteById(id);
+        String sucess = " Car " + id + " has been deleted ";
+        return sucess;
+    }
+
+    public Optional<Car> findById(@PathVariable Long id) {
+        Optional<Car> car = repository.findById(id);
+        return car;
+    }
+
+}
