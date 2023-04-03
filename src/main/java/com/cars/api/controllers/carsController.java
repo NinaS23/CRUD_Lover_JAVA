3b0eb71eca31c16a60b92acaa96015828601b19b
DUDA <mariabevenuto062@gmail.com>
2023-04-03 12:51:53 -0300

    feat: create car in h2 database

--- src/main/java/com/cars/api/controllers/carsController.java
+++ src/main/java/com/cars/api/controllers/carsController.java
@@ -1,23 +1,24 @@
 package com.cars.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cars.api.dto.CarsDTO;
+import com.cars.api.model.Car;
+import com.cars.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/api/cars")
 public class carsController {
-@PostMapping
-public void RegisterCar (@RequestBody CarsDTO req){
-    System.out.println( "\033[0;35m" + "\u001b[1mValor:\u001b[m" + req.valor());
-    System.out.println( "\033[0;35m" + "\u001b[1mDataDeFabricação:\u001b[m"  + req.dataFabricacao());
-    System.out.println( "\033[0;35m" + " \u001b[1mModelo:\u001b[m:" + req.modelo());
-    System.out.println( "\033[0;35m" + " \u001b[1mFabricante:\u001b[m" + req.fabricante());
-    System.out.println( "\033[0;35m" + " \u001b[1mAnoModelo:\u001b[m" + req.anoModelo());
- 
-}
-    
+    @Autowired
+    private CarRepository repository;
+
+    @PostMapping
+    public void RegisterCar(@RequestBody CarsDTO req) {
+        repository.save(new Car(req));
+    }
+
 }
