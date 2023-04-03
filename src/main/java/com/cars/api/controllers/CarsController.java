e6ac9f5fb8b1a0a0c95a0effa92fe258f7f009e4
DUDA <mariabevenuto062@gmail.com>
2023-04-03 16:39:51 -0300

    format code

--- src/main/java/com/cars/api/controllers/CarsController.java
+++ src/main/java/com/cars/api/controllers/CarsController.java
@@ -1,13 +1,17 @@
 package com.cars.api.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cars.api.dto.CarsDTO;
@@ -23,6 +27,7 @@ public class CarsController {
     private CarRepository repository;
 
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
     public void RegisterCar(@RequestBody @Valid CarsDTO req) {
         repository.save(new Car(req));
     }
@@ -34,11 +39,17 @@ public class CarsController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public String DeleteCarById(@PathVariable Long id) {
         repository.deleteById(id);
         String sucess = " Car " + id + " has been deleted ";
         return sucess;
     }
 
+    @GetMapping("/{id}")
+    public Optional<Car> findById(@PathVariable Long id) {
+        Optional<Car> car = repository.findById(id);
+        return car;
+    }
 
 }
